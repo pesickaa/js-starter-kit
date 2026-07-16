b916913053900c5817ccfc36dcdef390623401c0
Alexis Pesicka <14352901+pesickaa@users.noreply.github.com>
2026-07-16 16:22:03 +0100

    fix: update to pre-release for kinde-auth-pkce-js and fix errors

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,28 @@
 import "./style.css";
-import createKindeClient from "@kinde-oss/kinde-auth-pkce-js";
+import createKindeClient, { type KindeUser } from "@kinde-oss/kinde-auth-pkce-js";
 
 const loggedInViews = document.getElementsByClassName("js-logged-in-view");
 const loggedOutViews = document.getElementsByClassName("js-logged-out-view");
 
-const switchViews = (a, b) => {
+const switchViews = (a: HTMLCollectionOf<Element>, b: HTMLCollectionOf<Element>) => {
   [...a].forEach((v) => v.removeAttribute("hidden"));
-  [...b].forEach((v) => v.setAttribute("hidden", true));
+  [...b].forEach((v) => v.setAttribute("hidden", "true"));
 };
 
-const renderLoggedInView = (user) => {
+const renderLoggedInView = (user: KindeUser) => {
   const namePlaceholder = document.querySelector(".js-user-name");
   const avatarPlaceholder = document.querySelector(".js-user-avatar");
-  const avatarPicturePlaceholder = document.querySelector(
+  const avatarPicturePlaceholder = document.querySelector<HTMLImageElement>(
     ".js-user-avatar-picture"
   );
-  namePlaceholder.textContent = `${user.given_name} ${user?.family_name || ""}`;
+  if (namePlaceholder) {
+    namePlaceholder.textContent = `${user.given_name} ${user?.family_name || ""}`;
+  }
 
-  if (`${user.picture}` != "") {
+  if (`${user.picture}` != "" && avatarPicturePlaceholder) {
     avatarPicturePlaceholder.src = `${user.picture}`;
     avatarPicturePlaceholder.removeAttribute("hidden");
-  } else {
+  } else if (avatarPlaceholder && user.given_name) {
     avatarPlaceholder.textContent = `${user.given_name[0]}${
       user?.family_name?.[0] || user.given_name[1]
     }`;
@@ -36,7 +38,7 @@ const renderLoggedOutView = () => {
   switchViews(loggedOutViews, loggedInViews);
 };
 
-const render = async (user) => {
+const render = async (user: KindeUser | null | undefined) => {
   if (user) {
     renderLoggedInView(user);
   } else {
@@ -50,13 +52,15 @@ const kinde = await createKindeClient({
   redirect_uri: import.meta.env.VITE_KINDE_REDIRECT_URL,
 });
 
-const addKindeEvent = (id) => {
-  document.getElementById(id).addEventListener("click", async () => {
+type KindeAction = "login" | "register" | "logout";
+
+const addKindeEvent = (id: KindeAction) => {
+  document.getElementById(id)?.addEventListener("click", async () => {
     await kinde[id]();
   });
 };
 
-["login", "register", "logout"].forEach(addKindeEvent);
+(["login", "register", "logout"] as const).forEach(addKindeEvent);
 
 // Handle page load
 const user = await kinde.getUser();
